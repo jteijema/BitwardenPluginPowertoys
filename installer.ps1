36842b3cb83fdb1cac618b61040d4c0bebfcb546
Jelle Teijema <j.j.teijema@gmail.com>
2024-02-27 21:22:54 +0100

    Create installer.ps1

---
+++ installer.ps1
@@ -0,0 +1,25 @@
+# PowerShell script that downloads latest release from GitHub and installs it to PowerToys Run plugin folder
+# Usage: .\installer.ps1
+
+$installLocation = "%LOCALAPPDATA%\PowerToys\RunPlugins\BitwardenPlugin"
+
+# Get latest release from GitHub
+$latestRelease = Invoke-RestMethod -Uri "https://api.github.com/repos/jteijema/BitwardenPluginPowertoys/releases/latest"
+
+# Get download URL for latest release
+$downloadUrl = $latestRelease.assets.browser_download_url
+
+# Download latest release
+Invoke-WebRequest -Uri $downloadUrl -OutFile "BitwardenPlugin.tmp.zip"
+
+# Unzip latest release
+Expand-Archive -Path "BitwardenPlugin.tmp.zip" -DestinationPath "$installLocation.tmp"
+
+# Move files to plugin folder
+Move-Item "$installLocation.tmp\BitwardenPlugin" $installLocation
+
+# Remove zip file
+Remove-Item "BitwardenPlugin.tmp.zip"
+
+# Remove temporary folder
+Remove-Item "$installLocation.tmp" -Recurse
